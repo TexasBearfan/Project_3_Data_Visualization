5538a7d63d7e2674017daee6666a9f649908c7fa
TexasBearfan <goroos39@yahoo.com>
2024-03-14 20:02:07 -0500

    Cluster Map commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,30 @@
 
-const url='https://developer.nps.gov/api/v1/parks?parkCode=acad/alerts'
-full_url=url+'&api_key='+api_key
-console.log(full_url)
-d3.json(full_url).then(function(data){
-    console.log(data)
-})
+const url='https://developer.nps.gov/api/v1/parks?stateCode='
+let myMap = L.map("map", {
+    center: [30.7, -100.5],
+    zoom: 4
+  });
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+let markers=L.markerClusterGroup();
+state=['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+
+
+Coordinate_array=[]
+count=0
+for(let x=0;x<state.length;x++){
+state_url=url+state[x]+'&api_key='+api_key
+d3.json(state_url).then(function(item){
+    data=item.data
+    for(let i=0;i<data.length;i++){
+        row=data[i]
+        lat=row.latitude
+        long=row.longitude
+        markers.addLayer(L.marker([lat,long]).bindPopup(row.name));
+        count+=1
+    }
+});
+}
+myMap.addLayer(markers);
+console.log(count)
