63bb92be90f1cbba77ca225735262457c62d9fbb
TexasBearfan <goroos39@yahoo.com>
2024-03-19 20:34:55 -0500

    sorted values commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,14 +1,14 @@
 
 const url='https://developer.nps.gov/api/v1/parks?stateCode='
-let myMap = L.map("map", {
-    center: [30.7, -100.5],
-    zoom: 4
-  });
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-let markers=L.markerClusterGroup();
 
+let street=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+let markers=L.markerClusterGroup();
+let circles=[];
+function markersize(deaths){
+    return deaths*1000000;
+}
 
 
 Coordinate_array=[]
@@ -18,24 +18,48 @@ state_url=url+state[x]+'&api_key='+api_key
 d3.json(state_url).then(function(item){
     
     data=item.data
-    console.log(data)
+    
     for(let i=0;i<data.length;i++){
         row=data[i]
         park_status=parks.includes(row.fullName)
 
-        console.log(park_status)
+        
         if(park_status==true){
-            console.log(row)
-            
+            for(let m=0;m<parks.length;m++){
+                if(parks[m]==row.fullName){
+                    death_count=count_array[m]
+                    
+                }
+            }
             lat=row.latitude
             long=row.longitude
-            markers.addLayer(L.marker([lat,long]).bindPopup(row.name));
+            markers.addLayer(L.marker([lat,long]).bindPopup(row.name+" "+death_count));
+            circles.push(L.circle([lat,long],{
+                stroke:false,
+                fillOpacity:0.75,
+                color:'white',
+                fillColor:'red',
+                radius:markersize(death_count)
+            }))
         }
     }
+    
 });
 }
-myMap.addLayer(markers);
-
-
-
-
+console.log(circles)
+let circle_group=L.layerGroup(circles);
+let baseMaps={
+    'Cluster Map':markers,
+    'Circle':circle_group
+}
+let overlayMaps={
+    
+}
+let myMap = L.map("map", {
+    center: [30.7, -100.5],
+    zoom: 3,
+    layers:[street,markers]
+  });
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
