614a5303b1f4a76ac7d67503724b5062f399ea87
TexasBearfan <goroos39@yahoo.com>
2024-03-19 20:46:24 -0500

    circles update commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,11 +5,25 @@ let street=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 })
 let markers=L.markerClusterGroup();
-let circles=[];
-function markersize(deaths){
-    return deaths*1000000;
-}
+let circle_group=L.layerGroup();
 
+function markersize(deaths){
+    return deaths*950;
+}
+function determineColor(deaths){
+    if(deaths<10){
+        color='blue'
+    }
+    else if(deaths<20){
+        color='purple'
+    }
+    else if(deaths<100){
+        color='red'
+    }
+    else{
+        color='white'
+    }
+}
 
 Coordinate_array=[]
 name_array=[]
@@ -34,20 +48,19 @@ d3.json(state_url).then(function(item){
             lat=row.latitude
             long=row.longitude
             markers.addLayer(L.marker([lat,long]).bindPopup(row.name+" "+death_count));
-            circles.push(L.circle([lat,long],{
+            circle_group.addLayer(L.circle([lat,long],{
                 stroke:false,
                 fillOpacity:0.75,
-                color:'white',
-                fillColor:'red',
+                color:'black',
+                fillColor:determineColor(death_count),
                 radius:markersize(death_count)
-            }))
+            })).bindPopup(row.name+" "+death_count);
         }
     }
     
 });
 }
-console.log(circles)
-let circle_group=L.layerGroup(circles);
+
 let baseMaps={
     'Cluster Map':markers,
     'Circle':circle_group
