26ca70fc01d5bc82c3874c8d9bc9bf7004ab26d2
rhoadsc <carlie.r.rhoads@gmail.com>
2024-03-21 19:25:23 -0500

    updated html and css

--- File_Cleaning_data_extraction/LineChart.js
+++ File_Cleaning_data_extraction/LineChart.js
@@ -1,14 +1,14 @@
 // Define data for different national parks
 var parksData = {
-    "Yellowstone": [5, 8, 7, 10, 12, 9, 11, 15, 18, 16, 14, 13, 10, 9, 8],
-    "Yosemite": [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
-    "Grand Canyon": [2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4],
-    "Zion": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5],
-    "Glacier": [0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2]
+    "Lake Mead National Recreation Area": [5, 8, 7, 10, 12, 9, 11, 15, 18, 16, 14, 13, 10, 9, 8],
+    "Grand Canyon National Park": [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    "Yosemite National Park": [2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4],
+    "Natchez Trace Parkway": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5],
+    "Golden Gate National Recreation Area": [0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2]
 };
 // Create initial line chart
 var ctx = document.getElementById('lineChart').getContext('2d');
-var initialPark = "Yellowstone";
+var initialPark = "Lake Mead National Recreation Area";
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -41,9 +41,3 @@ function updateChart(selectedPark) {
     myChart.data.datasets[0].data = parksData[selectedPark];
     myChart.update();
 }
-
-
-
-
-
-1:55
